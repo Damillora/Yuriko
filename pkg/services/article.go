c7ff45976f93758a03812ce98afd40299fcdcd24
Damillora <damillora@damillora.com>
2021-05-07 21:37:05 +0700

    Add search fields

--- pkg/services/article.go
+++ pkg/services/article.go
@@ -19,7 +19,7 @@ func SearchArticles(q string, page int) (SearchResult, error) {
 	perPage := 10
 	searchParameters := &api.SearchCollectionParams{
 		Q:       q,
-		QueryBy: []string{"title"},
+		QueryBy: []string{"title","slug","primary_tag","excerpt","html"},
 		Page:    &page,
 		PerPage: &perPage,
 	}
